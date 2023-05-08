89261c35ba79234f0934624406f9d33331d2dde9
isdiscodead <isdiscodead@korea.ac.kr>
2023-05-08 20:25:46 +0900

    new project

---
+++ handle/src/screens/DetailScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Header } from '../components/Header/Header'
+import { View } from 'react-native/types'
+
+export const DetailScreen:React.FC = () => {
+  return (
+    <View style={{ flex: 1 }}>
+        <Header>
+            <Header.Title title="기록 추가하기" />
+            <Header.Icon iconName="close" onPress={() => {}} />
+        </Header>
+    </View>
+  )
+}
