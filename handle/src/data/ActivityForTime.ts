b0e76a3b7d3079400bce066abf45708a94f52860
isdiscodead <isdiscodead@korea.ac.kr>
2023-05-19 21:48:56 +0900

    add getActivityForTime

--- handle/src/data/ActivityForTime.ts
+++ handle/src/data/ActivityForTime.ts
@@ -6,3 +6,29 @@ export type ActivityForTime = {
     endTime: number;
     activity?: string;
 }
+
+const getActivityForTime = (time: number): ActivityForTime => {
+    if (time >= 23 || (time >= 0 && time <= 4)) {
+      return {
+        id: 1,
+        startTime: 23,
+        endTime: 4,
+        activity: "잠에 들 시간이 되었어요! 😴 아직 침대가 아니시라면 핸드폰을 꺼두고 누구보다 빠르게 취침 준비를 시작해볼까요?",
+      };
+      
+    } else if ( time >= 20 || time < 23 ) {
+        return {
+            id: 1,
+            startTime: 23,
+            endTime: 4,
+            activity: "잠에 들 시간이 되었어요! 😴 아직 침대가 아니시라면 핸드폰을 꺼두고 누구보다 빠르게 취침 준비를 시작해볼까요?",
+        };
+    }
+  
+    return {
+      startTime: time,
+      endTime: time + 1,
+      activity: "undefined",
+    };
+  };
+  
