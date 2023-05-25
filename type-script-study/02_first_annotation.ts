96198f3c877083c996cff011fe228b33b3fbfa39
isdiscodead <isdiscodead@korea.ac.kr>
2023-05-25 13:56:27 +0900

    first variable type annotation

---
+++ type-script-study/02_first_annotation.ts
@@ -0,0 +1,16 @@
+let a = "Mark";
+// typescript에서는 js와 달리 문자열 let -> 숫자 let으로 변경 불가능 ( 임의의 타입이 지정되었으므로 ) 
+// a = 39;
+a = "Steve";
+
+let b; // any 
+b = 3;
+b = "Call";
+
+let c: string; // string으로 지정
+c = "Mark";
+// c = 39;
+
+let d: number; // number로 지정 
+// d = "Mark";
+d = 39;
