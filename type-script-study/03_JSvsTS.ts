43d61321b1bb246d7d983200b78d96f8b5ffcb26
isdiscodead <isdiscodead@korea.ac.kr>
2023-05-29 14:24:07 +0900

    typescript vs javascript : js

---
+++ type-script-study/03_JSvsTS.ts
@@ -0,0 +1,14 @@
+// JavaScript 
+/*
+function add( n1, n2 ) {
+    if ( typeof n1 !== 'number' || typeof n2 !== 'number' ) {
+        throw new Error("Incorrect input!");
+    }
+    return n1 + n2;
+}
+
+const result = add(32, 39);
+ */
+
+
+// TypeScript
