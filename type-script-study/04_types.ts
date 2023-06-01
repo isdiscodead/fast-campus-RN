bdfd25f76a85783e20c075e1609fef92fcbac552
isdiscodead <isdiscodead@korea.ac.kr>
2023-06-01 14:50:14 +0900

    -

--- type-script-study/04_types.ts
+++ type-script-study/04_types.ts
@@ -1,2 +1,6 @@
 // Primitive Types
 // 오브젝트와 레퍼런스 형태가 아닌 실제 값을 저장하는 타입 
+// 이러한 프리미티브 형에서 내장 함수를 사용 가능한 것은 자바스크립트의 처리 방식 덕분! 
+// boolean, number, string, symbol, null, undefined 
+let name2 = "mark";
+name2.toString();
