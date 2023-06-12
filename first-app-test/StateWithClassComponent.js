00cc8c0bb7b436d622395dc29edafe80ecd43371
isdiscodead <isdiscodead@korea.ac.kr>
2023-06-13 06:28:00 +0900

    -

--- first-app-test/StateWithClassComponent.js
+++ first-app-test/StateWithClassComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, View, Text, Button } from "react";
 
 export default class StateWithClassComponent extends Component {
   constructor(props) {
--- first-app-test/StateWithFunctionalComponent.js
+++ first-app-test/StateWithFunctionalComponent.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, View, Text, Button } from 'react';
 
 function StateWithFunctionalComponent() {
 
   const [count, setCount] = useState(0);
 
+  const arr = [1, 2, 3];
+  const [first, second, third] = arr;
+
   return ( 
     <View>
-      <Text>You Clicked {this.state.count} times</Text>
+      <Text>You Clicked {count} times</Text>
       <Button 
         title="Click Me"
         onPress={()=>{ setCount(count + 1) }}
