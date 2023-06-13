f1ab715dede9827d4f5c6f21fde45d661c0f7894
isdiscodead <isdiscodead@korea.ac.kr>
2023-06-14 06:35:20 +0900

    useEffect end

--- first-app-test/StateWithFunctionalComponent.js
+++ first-app-test/StateWithFunctionalComponent.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { useState, View, Text, Button } from 'react';
+import { Switch, TextInput } from 'react-native';
 
 function StateWithFunctionalComponent() {
 
-  const [count, setCount] = useState(0);
-
-  const arr = [1, 2, 3];
-  const [first, second, third] = arr;
+  const [count, setCount] = useState(0); 
+  const [isOn, setIsOn] = useState(false);
+  const [name, setName] = useState("");
 
   return ( 
     <View>
@@ -14,6 +14,24 @@ function StateWithFunctionalComponent() {
       <Button 
         title="Click Me"
         onPress={()=>{ setCount(count + 1) }}
+      />
+
+      <Text>----------------</Text>
+      <Switch
+        value={isOn}
+        onValueChange={v => {
+          console.log('v', v);
+          setIsOn(v);
+        }}
+      />
+
+      <Text>----------------</Text>
+      <TextInput 
+        value={name}
+        onChangeText={v => {
+          console.log('v', v);
+          setName(v);
+        }}
       />
     </View>
   );
